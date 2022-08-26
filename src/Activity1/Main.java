8e5703353ae8ac63c6c6a13c90d68a85fd45c88a
N4choCM <nachocamposmarti@gmail.com>
2022-08-26 16:20:33 +0200

    Activity 1

---
+++ src/Activity1/Main.java
@@ -0,0 +1,14 @@
+package Activity1;
+
+public class Main {
+    public static void main(String[] args) {
+        int iPhone = 628523682;
+        Long lId = 1L;
+        double dNum = 1.52;
+        boolean bMarried = false;
+        String sName = "Nacho";
+        System.out.println("Mi número de usuario es " + lId + ", mi número de teléfono es el " + iPhone + ", " +
+                "mi número decimal favorito es el " + dNum + ", mi booleano es el " +
+                bMarried + " (que significa que no estoy casado), y me llamo " + sName + ".");
+    }
+}

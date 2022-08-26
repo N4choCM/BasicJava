689a74b8d6399c42e8285c443325f5dd824392e0
N4choCM <nachocamposmarti@gmail.com>
2022-08-26 16:40:24 +0200

    Activity 3

---
+++ src/Activity3/Activity3.java
@@ -0,0 +1,11 @@
+package Activity3;
+
+public class Activity3 {
+    public static void main(String[] args) {
+        String[] arrayNames = new String[]{"Laura", "Nacho", "Juan", "Javier", "Fina", "Ramón"};
+
+        for (String name : arrayNames) {
+            System.out.println(name);
+        }
+    }
+}
